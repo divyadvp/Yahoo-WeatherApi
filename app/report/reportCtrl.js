c40c9dd14371ff8f8ab57f932794be9c79b8b4db
venkatesh devarapalli <venkatesh devarapalli>
2016-11-12 19:35:38 +0530

    Hiding data

--- app/report/reportCtrl.js
+++ app/report/reportCtrl.js
@@ -2,14 +2,16 @@
     function reportFn($scope,reportSvc){
         $scope.cityName="";
         $scope.stateName="";
+        $scope.Visible = false;
         $scope.searchData=function(){
        reportSvc.getWeatherFromApi($scope.cityName,$scope.
                                    stateName)
         
         .then(function(response){
             console.log(response);
-            $scope.data=response;
-           /*.data.query.results.channel*/
+            $scope.data=response.data.query.results.channel;
+           $scope.Visible = true;
+        
         })
         .catch(function(error){
             console.log("URL Incorrect May be");
